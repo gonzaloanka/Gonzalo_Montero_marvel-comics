0bc93ec07c6a3c3bc2a331c228c3862ade6e45e8
gonzaloanka <149703470+gonzaloanka@users.noreply.github.com>
2024-11-15 14:04:00 +0100

    Update ComicDetail.jsx

--- src/components/ComicDetail.jsx
+++ src/components/ComicDetail.jsx
@@ -1,3 +1,4 @@
+/**/
 import React, { useEffect, useState } from 'react';
 import { fetchComicDetail } from '../marvelApi';
 import { saveToFavorites, removeFromFavorites, getFavorites } from '../utils/localStorageUtils';
