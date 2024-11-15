a45d510215c6235407c5a94334df093f7e63e7fd
gonzaloanka <149703470+gonzaloanka@users.noreply.github.com>
2024-11-15 11:13:04 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ComicList from './components/ComicList';
+import ComicDetail from './components/ComicDetail';
+import FavoriteComics from './components/FavoriteComics';
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <nav>
+                <Link to="/">Inicio</Link> {/* Enlace a la página principal */}
+                <Link to="/favorites">Favoritos</Link> {/* Enlace a la página de favoritos */}
+            </nav>
+            <Routes>
+                <Route path="/" element={<ComicList />} /> {/* Ruta para la lista de cómics */}
+                <Route path="/comic/:comicId" element={<ComicDetail />} /> {/* Ruta para el detalle del cómic */}
+                <Route path="/favorites" element={<FavoriteComics />} /> {/* Ruta para la lista de favoritos */}
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
+
+
+
